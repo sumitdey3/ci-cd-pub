29214cb0f412674318b62576f885857f20225e16
sumitdey3 <sumitdey.sr.204@gmail.com>
2020-06-21 15:09:54 +0530

    testing 1

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 #!/usr/bin/env python2
 
-from <myprojectFILE> import app
+#from <myprojectFILE> import app
+from flask_website import app
 
 if __name__ == "__main__":
     app.run()
